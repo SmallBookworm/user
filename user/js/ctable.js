3663433bf38081714cd6ff9cf961b8584585ee82
Peng <605144885@qq.com>
2017-07-24 17:34:29 +0800

    3

--- user/js/ctable.js
+++ user/js/ctable.js
@@ -19,3 +19,37 @@ var json = [{
 		状态: '在线'
 	}
 ];
+
+function createTable(json) {
+	var ctable = document.getElementById('ctable');
+	ctable.innerHTML = '';
+	init(json, ctable);
+
+	for(var i in json) {
+		var tr = document.createElement('tr');
+		for(var j in json[i]) {
+			var td = document.createElement('td');
+			td.textContent = json[i][j];
+			//			td.style.width = '100px';
+			tr.appendChild(td);
+		}
+		ctable.appendChild(tr);
+	}
+}
+
+function init(json, ctable) {
+	var thr = document.createElement('tr');
+	for(var i in json[0]) {
+		var th = document.createElement('th');
+		th.textContent = i;
+		thr.appendChild(th);
+	}
+	ctable.appendChild(thr);
+}
+
+function createExcel(table,  name) {
+	var blob = new Blob(['<html><head><meta charset="UTF-8"></head><body><table>' + document.getElementById(table).innerHTML + '</table></body></html>'], {
+		type: "text/plain;charset=utf-8"
+	});
+	saveAs(blob, name + ".xls");
+}
